c128a549b0fd1832764f1839d7dad87876932226
flym <www@iflym.com>
2018-11-09 17:57:31 +0800

    修正切面中可能拿不到列信息的问题

--- iflym-mybatis/src/main/java/io/iflym/mybatis/domain/aspect/UpdatableAspect.java
+++ iflym-mybatis/src/main/java/io/iflym/mybatis/domain/aspect/UpdatableAspect.java
@@ -3,8 +3,13 @@ package io.iflym.mybatis.domain.aspect;
 import io.iflym.core.util.ObjectUtils;
 import io.iflym.mybatis.domain.Entity;
 import io.iflym.mybatis.domain.Updatable;
+import io.iflym.mybatis.domain.info.ColumnInfo;
+import io.iflym.mybatis.domain.info.EntityInfo;
 import io.iflym.mybatis.domain.info.EntityInfoHolder;
 import io.iflym.mybatis.domain.util.UpdateUtils;
+import io.iflym.mybatis.exception.MybatisException;
+import lombok.experimental.var;
+import lombok.val;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,6 +18,7 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -49,10 +55,10 @@ public class UpdatableAspect {
         }
 
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Class clazz = self.getClass();
+        Class<?> clazz = self.getClass();
 
-        String fieldName = methodSignature.getParameterNames()[0];
-        Field property = EntityInfoHolder.get(clazz).getColumn(fieldName).getField();
+        val columnInfo = getColumn(clazz, EntityInfoHolder.get(clazz), methodSignature);
+        Field property = columnInfo.getField();
         ReflectionUtils.makeAccessible(property);
 
         Object currentOldValue = property.get(self);
@@ -62,5 +68,23 @@ public class UpdatableAspect {
         }
 
         joinPoint.proceed();
+    }
+
+    private <T extends Entity> ColumnInfo getColumn(Class<?> clazz, EntityInfo<T> entityInfo, MethodSignature signature) {
+        String name = signature.getParameterNames()[0];
+        var column = entityInfo.getColumn(name);
+        if(column != null) {
+            return column;
+        }
+
+        val methodName = signature.getName();
+        val methodNamePostfix = methodName.substring(3);
+
+        //没拿到，其可能是kotlin类，也可能是编译上的问题，导致没有拿到名字信息，这里重新从列信息中查找路由
+        List<ColumnInfo> columnList = entityInfo.getColumnList();
+        return columnList.stream()
+                .filter(t -> t.getPropertyName().equalsIgnoreCase(methodNamePostfix))
+                .findFirst()
+                .orElseThrow(() -> new MybatisException("不能通过方法找到相应的列名:" + methodName));
     }
 }
